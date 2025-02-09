6e98df9eaa0c1cc9b4bbbad3cf426d694af3a09a
soomlir <soomlir@yandex.ru>
2025-02-09 20:00:13 +0300

    main edit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router";
 import './style.css'
 import App from './App.vue'
-import IndexPage from './pages/IndexPage.vue';
-import CatalogPage from './pages/CatalogPage.vue';
+import IndexPage from '@/pages/IndexPage.vue';
+import CatalogPage from '@/pages/CatalogPage.vue';
 
 
 const router = createRouter({
